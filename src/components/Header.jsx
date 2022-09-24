b4c340714156fb1f184be6b8306bde360120bb68
Mohit <mohitsharma8864@gmail.com>
2022-09-25 00:29:48 +0530

    create container category and image section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,8 +88,12 @@ const Header = () => {
                          className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0">
                             {user && user.email === "mohitsharma8864@gmail.com" && (
                                 <Link to={'/createItem'}>
-                                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                                    transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd/></p>  
+                                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer
+                                    hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor 
+                                    text-base "
+                                    onClick={() => setIsMenu(false)}
+                                    >
+                                    New Item <MdAdd/></p>  
                                 </Link>  
                             )}
                             <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
@@ -140,10 +144,10 @@ const Header = () => {
                         )}
 
                         <ul className='flex flex-col'>
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li>
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menu</li>
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
-                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li>
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Home</li>
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Menu</li>
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>About Us</li>
+                            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Service</li>
                         </ul>
 
                         <p className="m-2 p-2 rounded-md shadow-md flex items-center 
--- src/utils/data.js
+++ src/utils/data.js
@@ -11,7 +11,6 @@ export const heroData = [
         price: '5.25', 
         imageSrc: I1,
     },
-
     {
         id : 2, 
         name: 'Strawberries', 
@@ -19,7 +18,6 @@ export const heroData = [
         price: '10.25', 
         imageSrc: F1,
     },
-
     {
         id : 3, 
         name: 'Chicken Kebab', 
@@ -27,7 +25,6 @@ export const heroData = [
         price: '5.25', 
         imageSrc: C3,
     },
-
     {
         id : 4, 
         name: 'Fish Kebab', 
@@ -35,5 +32,42 @@ export const heroData = [
         price: '5.25', 
         imageSrc: Fi1,
     },
+]
 
+export const categories = [
+    {
+        id: 1,
+        name: "Chicken",
+        urlParamName: "chicken",
+    },
+    {
+        id: 2,
+        name: "Curry",
+        urlParamName: "curry",
+    },
+    {
+        id: 3,
+        name: "Rice",
+        urlParamName: "rice",
+    },
+    {
+        id: 4,
+        name: "Fish",
+        urlParamName: "fish",
+    },
+    {
+        id: 5,
+        name: "Fruits",
+        urlParamName: "fruits",
+    },
+    {
+        id: 6,
+        name: "Icecreams",
+        urlParamName: "icecreams",
+    },
+    {
+        id: 7,
+        name: "Soft Drinks",
+        urlParamName: "drinks",
+    },
 ]
