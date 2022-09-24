cf5d0e17f8614cd202507915b593015fd0a27517
Mohit <mohitsharma8864@gmail.com>
2022-09-25 00:56:38 +0530

    create container form ui

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import {MdFastfood, MdCloudUpload, MdDelete} from 'react-icons/md';
+import {MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney} from 'react-icons/md';
 import { categories } from '../utils/data';
 import Loader from './Loader';
 
@@ -18,7 +18,7 @@ const CreateContainer = () => {
 
   const uploadImage = () => {};
   const deleteImage = () => {};
-
+  const saveDetails = () => {};
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -46,7 +46,7 @@ const CreateContainer = () => {
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               placeholder="Give me a title..."
-              className='w-full h-full text-lg bg-transparent font-semibold
+              className='w-full h-full text-lg bg-transparent
               outline-none border-none placeholder:text-gray-400 text-textColor'
             />
           </div>
@@ -79,6 +79,7 @@ const CreateContainer = () => {
                 ) : (
                   <>
                     {!imageAsset ? (
+
                       <>
                         <label className='w-full h-full flex flex-col items-center
                         justify-center cursor-pointer'>
@@ -122,7 +123,41 @@ const CreateContainer = () => {
                )}
           </div>
   
-          <div className='w-full flex flex-col md:flex-row items-center gap-3' ></div>
+          <div className='w-full flex flex-col md:flex-row items-center gap-3'>
+            <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+              <MdFoodBank className='text-gray-700 text-2xl'/>
+              <input
+                type="text" 
+                required 
+                value={calories}
+                onChange={(e) => setCalories(e.target.value)}
+                placeholder="Calories"
+              className='w-full h-full text-lg bg-transparent outline-none 
+              border-none placeholder:text-gray-400 text-textColor'
+              />
+            </div>
+
+            <div className='w-full py-2 border-b border-gray-300 flex items-center gap-2'>
+              <MdAttachMoney className='text-gray-700 text-2xl'/>
+              <input
+                type="text" 
+                required 
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Price"
+              className='w-full h-full text-lg bg-transparent outline-none 
+              border-none placeholder:text-gray-400 text-textColor'
+              />
+            </div>
+          </div>
+          
+          <div className='flex items-center w-full'>
+            <button type='button' className='ml-0 md:ml-auto w-full md:w-auto border-none 
+            outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold'
+            onClick={saveDetails}
+          > Save
+            </button>
+          </div>
       </div>
     </div>
   );
